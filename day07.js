231ed8d9ab5b30dfa3f1255861e9374c04c5e14a
DEVAVISWA G <140805126+DEVAVISWA@users.noreply.github.com>
2023-09-13 01:01:23 +0530

    Add files via upload

---
+++ day07.js
@@ -0,0 +1,78 @@
+// 1 a
+// var country= new XMLHttpRequest();
+// country.open("GET","https://restcountries.com/v3.1/all");
+// country.send();
+// country.onload= function(){
+//     const countries=JSON.parse(country.response)
+//     // console.log(countries)
+//     const filteredArray = countries.filter((v) => {
+//         return v.region == "Asia"
+//     });
+//     console.log(filteredArray)
+    
+// }
+
+//1 b
+// var country= new XMLHttpRequest();
+// country.open("GET","https://restcountries.com/v3.1/all");
+// country.send();
+// country.onload= function(){
+//     const countries=JSON.parse(country.response)
+//     console.log(countries)
+//     const filteredArray = countries.filter((v) => {
+//         return v.population < 200000
+//     });
+//     console.log(filteredArray)
+    
+// }
+
+//1 c
+// var country= new XMLHttpRequest();
+// country.open("GET","https://restcountries.com/v3.1/all");
+// country.send();
+// country.onload= function(){
+//     const countries=JSON.parse(country.response)
+//     // console.log(countries)
+//     const forEach =countries.forEach((v,i,aA) => {
+//         for (let i=0;i<countries.length;i++){
+//         console.log(`${countries[i].name}
+//         ${countries[i].capital}
+//         ${countries[i].flag}`)
+//         }
+//     });
+    
+// }
+
+//1 d
+// var country= new XMLHttpRequest();
+// country.open("GET","https://restcountries.com/v3.1/all");
+// country.send();
+// country.onload= function(){
+//     const countries=JSON.parse(country.response)
+//     // console.log(countries)
+//     const filteredArray = countries.filter((acc,v) => {
+//         for(let i=0 ;i<countries.length;i++) {
+//         console.log (countries[i].population) 
+//     }
+//     },0);
+//     console.log(filteredArray)
+    
+// }
+
+//1 e
+// var country= new XMLHttpRequest();
+// country.open("GET","https://restcountries.com/v3.1/all");
+// country.send();
+// country.onload= function(){
+//     const countries=JSON.parse(country.response)
+//     // console.log(countries)
+//     const filteredArray = countries.filter((v) => {
+//         for(let i=0 ;i<countries.length;i++) {
+//         console.log(countries[i].currencies.USD)
+        
+//     }
+//     });
+//     console.log(filteredArray)
+    
+// }
+
